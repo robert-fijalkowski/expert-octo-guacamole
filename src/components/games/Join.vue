bfe74744f4fe26ab3757b967aa9c1b5fed306ae3
Mateusz Odelga <mateusz.odelga@schibsted.pl>
2017-12-29 11:59:52 +0100

    Fix Join issue

--- src/components/games/Join.vue
+++ src/components/games/Join.vue
@@ -4,12 +4,12 @@
       <p class="title is-centered">{{title}}</p>
       <p class="subtitle"> {{game.name}}, {{game.location}} </p>
     </header>
-    <section class="modal-card-body  is-primary">
+    <section class="modal-card-body is-primary">
       <Search :predata="change ? [change] : null" :preselect="change ? change.id : null" :has-deep-search="false" @selected="selected" simple :limit="21" />
     </section>
     <footer class="modal-card-foot">
       <button class="button is-danger" type="button" @click="$parent.close(false)">Abandon</button>
-      <button class="button is-warning is-pulled-right" @click="join" v-if="club && club.id !== change.id">
+      <button class="button is-warning is-pulled-right" @click="join" v-if="club && (!change || club.id !== change.id)">
         {{ action }} {{ club.name }}
       </button>
     </footer>
@@ -27,7 +27,7 @@ export default {
     gameId: String,
     fullGame: Object,
     id: String,
-    change: { type: Object, default: null },
+    change: { type: Object, default: () => null },
   },
   data() {
     return { game: {}, club: this.change };
